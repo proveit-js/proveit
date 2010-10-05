1655ed1d937398f217ea0b9dbef97f034eebd653
Terris Johnson <tjohnson3@mail.gatech.edu>
2010-10-04 23:39:11 -0400

    User, Dev, About Pages completed. Images for all sections. Edit to footer; report-a-bug link was incorrectly linked.

--- website/developers.php
+++ website/developers.php
@@ -1,44 +1,25 @@
 <?php
 
-$page = 'About';
+$page = 'Developers';
 
 include_once 'header.php';
 
 ?>
 			<!-- Main Content Table -->
 
-				<div id="leadInPic"><img src="img/developers_leadIn.jpg" alt="about us banner" /></div>
+				<div id="leadInPic"><img src="img/developers_leadIn.jpg" alt="developers banner" /></div>
 
-				<table width="960" border="0" cellspacing="0" cellpadding="0" style="background-color: #2682c7; ">
+				<table border="0" cellspacing="0" cellpadding="0" style="background-color: #2682c7; ">
 					<tr>
 						<td>
-							<div id="940pic" class="any">
-								<a href="documentation.php">
-									<img src="img/documentation_image_link.jpg">
-								</a>
-								<h1> Documentation </h1>
-							</div>
-						
-							 <div id="465pic" class="left"> 
-								<a href="#"> 
-									<img  src="img/gCode_image_link.jpg"> 
-								</a> 
-								<h1>Google Code</h1>
-							</div>
-							
-							 <div id="465pic" class="right"> 
-								<a href="#"> 
-									<img  src="img/wiki_image_link.jpg"> 
-								</a> 
-								<h1>Wiki</h1>
-							</div> 
-								
-							<div id="mainBody"> 
-								<p class="leadIn">If there is any text, it will go here.</p>
-								
-							</div><!-- end #mainBody -->
+							<a class="pic940" style="background-image: url(img/documentation_image_link.jpg);" href="documentation.php">Documentation</a>
 						</td>
-						
+					</tr>
+					<tr>
+						<td>
+							<a class="pic465 left" style="background-image: url(img/gCode_image_link.jpg);" href="#">Google Code</a>
+							<a class="pic465 right" style="background-image: url(img/wiki_image_link.jpg);" href="#">Wiki</a>
+						</td>
 					</tr>
 				</table> 
 
