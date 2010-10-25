8cd0d46b4f0c812b1d0c4a2279146dfc7bc1e7e2
luther <>
2010-10-25 18:40:39 -0400

    * missing line break

--- website/users.php
+++ website/users.php
@@ -6,11 +6,12 @@ include_once 'header.php';
 
 ?>
 		<div id="portal">
-			<a class="pic940" 	style="background-image: url(img/features_image_link.jpg);" href="features.php">Features</a>
+			<a class="pic940" style="background-image: url(img/features_image_link.jpg);" href="features.php">Features</a>
 			<a class="pic465 left" style="background-image: url(img/screenshots_image_link.jpg);" href="screenshots.php">Screenshots</a>
 			<a class="pic465 right" style="background-image: url(img/tutorials_image_link.jpg);" href="tutorials.php">Tutorials</a>
-			<a class="pic465 left" style="background-image: url(img/reportabug_image_link.jpg);" href="reportabug.php">Report-a-Bug</a>
+			<a class="pic465 left" style="background-image: url(img/reportabug_image_link.jpg);" href="reportabug.php">Report a Bug</a>
 			<a class="pic465 right" style="background-image: url(img/demo_image_link.jpg);" href="demo.php">Demo</a>
+			<br />
 		</div>
 
 <?php include_once 'footer.php'; ?>
