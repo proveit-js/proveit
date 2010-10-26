d5e1e7523b72692b8b08cb9a449f7cba1a42a608
luther <>
2010-10-26 16:53:01 -0400

    tweaks to Report a Bug page

--- website/reportabug.php
+++ website/reportabug.php
@@ -8,62 +8,57 @@ include_once 'header.php';
 
 			<!-- Main Content Table -->
 
-				<div id="leadInPic"><img src="img/reportabug_leadIn.jpg" alt="doc_banner" /></div>
-
-				<table width="960" border="0" cellspacing="0" cellpadding="0" style="background-color: #2682c7; ">
+				<table id="mainTable">
 					<tr>
-						<td style="width: 686px;">
+						<td id="mainContent">
 
 							<div id="mainPic" class="reportabug">
 								<h1>Report a Bug</h1>
 							</div><!-- end #mainPic -->
 							<div id="mainBody"> 
-								<h3>How Do I Report a Bug?</h3>
-								<h4 id="GettingStarted">Getting Started</h4>
+								<h2>How Do I Report a Bug?</h2>
+								
+								<h3 id="GettingStarted">Getting Started</h3>
 								<p>All users are encouraged to report any bugs (or glitches) encountered while using ProveIt. Any and all help is appreciated, and will be used to further improve the ProveIt experience.</p>
-								<h4 id="Tips">Tips for Writing Bugs</h4>
-								<p>To help the ProveIt developers fix the bug, you should provide us with the information we need to reproduce it.  This will typically include:</p>
-								<ol>
+								
+								<h3 id="Fields">Issue Tracker</h3>
+								<p>We use Google Code's issue tracker to manage bug reports. To create a new bug report, go to the <a href="http://code.google.com/p/proveit-js/issues/entry?template=Defect%20report%20from%20user">New Issue</a> page. You'll see a number of fields:</p>
+								<dl>
+									<dt>Summary</dt>
+									<dd>A brief, one-line summary of the problem you're having.</dd>
+									<dt>Description</dt>
+									<dd>A detailed description of your problem. See our <a href="#Tips">tips for writing good bug reports</a> below.</dd>
+									<dt>Status</dt>
+									<dd>The status of the bug. Your report will initially be marked <em>New</em>.  If it is verified, it will be changed to <em>Accepted</em>.  When we have begun working, it will be changed to <em>Started</em>.  Finally, when work is complete, it will be marked <em>Fixed</em>.</dd>
+									<dt>Label(s)</dt>
+									<dd>One or more short tags used to categorize the report. We use various labels help organize reports, including <em>Type</em> (e.g. Defect, Enhancement, or Task), <em>Priority</em>, and <em>OpSys</em> (operating system).</dd>
+									<dt>Owner</dt>
+									<dd>The developer responsible for monitoring your report. You should post any relevant information to the issue page, rather than contacting the developer directly.</dd>
+								</dl>
+								
+								<p>Once you've filled out the Status and Description fields, click the "Submit issue" button. You're done! Thanks in advance.</p>
+
+								<h3 id="Tips">Tips for Writing Bug Reports</h3>
+								<p>To help the ProveIt developers fix the bug, please provide enough information for them to reproduce it. This will typically include:</p>
+								<ul>
 									<li>Your browser and its version.  For example, Firefox 3.6.10, Internet Explorer 8, or Chrome 7.0.517.24</li>
 									<li>The article you were editing</li>
 									<li>The expected result</li>
 									<li>The problem you experienced</li>
-								</ol>
-								<h4 id="Fields">Issue Tracker Fields</h4>
-								<dl>
-									<dt>Status</dt>
-									<dd>- The status of the bug.  Your issue will initially be marked New.  If it is verified, it will be changed to Accepted.  When we have begun working, it will be changed to Started.  Finally, when work is complete, it will be marked Fixed.</dd>
-									<dt>Label</dt>
-									<dd>- Various labels are used to help organize issues.  These include Type (e.g. Defect, Enhancement, or Task), Priority, and OpSys (operating system).</dd>
-									<dt>Owner</dt>
-									<dd>- This indicates the person with responsibility for monitoring the issue.  However, you should post any relevant information to the issue page, rather than contacting them directly.</dd>
-								</dl>
+								</ul>								
+								
+								<h2><a href="http://code.google.com/p/proveit-js/issues/entry?template=Defect%20report%20from%20user">Report a Bug Now</a></h2>
 
-								<h2><a href="http://code.google.com/p/proveit-js/issues/entry">Report a Bug Now</a></h2>
-								<!--table>      
-									<tr>
-										<td style="background-color: #2682c7; ">
-											<div id="recommendationHeader"> RECOMMENDED TUTORIALS </div> 
-											<div class="recommend left"> <div class="recommendText firstLine"> Getting Started with</div>
-											<div class="recommendText secondLine"><a href="" class="recommendLink"> ProveIt</a></div></div> 
-		
-											<div class="recommend right"> <div class="recommendText firstLine"> Quick Interface </div>
-											<div class="recommendText secondLine"> <a href="features.html" class="recommendLink"> WALKTHROUGH </a></div></div>
-										</td>
-									</tr>
-								</table -->     
 							</div><!-- end #mainBody -->
 						</td>
-						<td> 
-							<h2> Contents </h2>
-							<div id="sideTableofContents">
-								<h3 class="first">How Do I Report a Bug?</h3>
-								<ul>
-									<li><a href="#GettingStarted">Getting Started</a></li>
-									<li><a href="#Tips">Tips for Writing Bugs</a></li>
-									<li><a href="#Fields">Issue Tracker Fields</a></li>
-								</ul>
-							</div>
+						<td id="sideTableofContents"> 
+							<h2>Report a Bug </h2>
+							<ul>
+								<li><a href="#GettingStarted">Getting Started</a></li>
+								<li><a href="#Fields">Issue Tracker</a></li>
+								<li><a href="#Tips">Tips for Writing Bug Reports</a></li>
+								<li><a href="http://code.google.com/p/proveit-js/issues/entry?template=Defect%20report%20from%20user">Report a Bug Now</a></li>
+							</ul>
 					  </td>
 					</tr>
 				</table> 
