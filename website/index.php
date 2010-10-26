efe99378a9b2443163454e40f6445e835817d905
Terris Johnson <tjohnson3@mail.gatech.edu>
2010-10-26 12:11:00 -0400

    - Front page Image, Styling and Text Adjustments  (Starting to look official...lol)

--- website/index.php
+++ website/index.php
@@ -16,19 +16,19 @@ include_once 'header.php';
 						<td id="mainContent">
 
 							<div id="mainPic" class="index">
-								<h1>Available 555-5501</h1>
+								<h1>NOW IT'S EASY</h1>
 							</div><!-- end #mainPic -->
 							<div id="mainBody"> 
-								<h2>What is ProveIt?</h2>
+								<h2>Wikipedia Referencing Simplified</h2>
 
 								<p>ProveIt is an open-source, reliable referencing tool for Wikipedia. ProveIt makes finding, adding, and editing references on Wikipedia simple and easy all for <strong>FREE</strong>. ProveIt only needs to be installed once and can be accessed from any computer, any web broswer.</p>	
 							
 							<h2>Why Would I Use ProveIt?</h2>
 						
-							<p>ProveIt is for anyone who creates, edits, or needs to access references on Wikipedia articles. ProveIt makes locating, adding, and editing references easy by eliminating the need to learn Wikipedia coding standards. </p>
+							<p>ProveIt is for anyone who creates, edits, or needs to access references from Wikipedia articles. ProveIt makes locating, adding, and editing references easy by providing a simple interface for managing all referencing tasks. Feel free to <strong><a href="demo.php">DEMO PROVEIT</a></strong> here on the site to put your anxiety to rest.</p>
 							
 							<h2> How Do I Get Started? </h2>
-							<p> This is as simple as it gets! ProveIt is a quick copy-and-paste install, requiring no additional download or files to your computer.<strong><a href="install.php">GET PROVEIT</a></strong> now, or if you would like to give ProveIt a <strong><a href="demo.php">TEST DRIVE</a></strong> live on this site.</p>
+							<p> This is as simple as it gets! ProveIt is a quick copy-and-paste install, requiring no additional downloads or files. You only need a Wikipedia User Account -- if you do not have one already. Go ahead and<strong><a href="install.php">GET PROVEIT</a></strong> now for further instructions, or give ProveIt a <strong><a href="demo.php">TEST DRIVE</a></strong> before setting up.</p> 
 			
    
 							</div><!-- end #mainBody -->
