41bd25f7c0b0014d3735296a48fbf3b6cb188b6e
luther <>
2010-10-26 12:54:10 -0400

    * tweaking home page verbiage * removing gCode.php and wiki.php

--- website/index.php
+++ website/index.php
@@ -20,16 +20,13 @@ include_once 'header.php';
 							</div><!-- end #mainPic -->
 							<div id="mainBody"> 
 								<h2>Wikipedia Referencing Simplified</h2>
+								<p>ProveIt is a free, open-source referencing tool that makes it that makes it really easy to find, edit, add, and cite references in Wikipedia articles. ProveIt only needs to be installed once and can be accessed from any computer and any web broswer.</p>	
 
-								<p>ProveIt is an open-source, reliable referencing tool for Wikipedia. ProveIt makes finding, adding, and editing references on Wikipedia simple and easy all for <strong>FREE</strong>. ProveIt only needs to be installed once and can be accessed from any computer, any web broswer.</p>	
-							
-							<h2>Why Would I Use ProveIt?</h2>
-						
-							<p>ProveIt is for anyone who creates, edits, or needs to access references from Wikipedia articles. ProveIt makes locating, adding, and editing references easy by providing a simple interface for managing all referencing tasks. Feel free to <strong><a href="demo.php">DEMO PROVEIT</a></strong> here on the site to put your anxiety to rest.</p>
-							
-							<h2> How Do I Get Started? </h2>
-							<p> This is as simple as it gets! ProveIt is a quick copy-and-paste install, requiring no additional downloads or files. You only need a Wikipedia User Account -- if you do not have one already. Go ahead and<strong><a href="install.php">GET PROVEIT</a></strong> now for further instructions, or give ProveIt a <strong><a href="demo.php">TEST DRIVE</a></strong> before setting up.</p> 
-			
+								<h2>By Wikipedians, For Wikipedians</h2>
+								<p>ProveIt was created by a group of Wikipedia editors, researchers, and fans. We know Wikipedia, we love Wikipedia, and want want to make it better -- for us and everyone else, especially new users.</p>
+								
+								<h2>Getting Started</h2>
+								<p> ProveIt is a quick copy-and-paste install, requiring no additional downloads or files. All you need is a free Wikipedia user account -- if you don't have one already. Check out our 4-step <strong><a href="install.php">install guide</a></strong> for more info, or give ProveIt a <strong><a href="demo.php">test drive</a></strong> to see for yourself.</p> 
    
 							</div><!-- end #mainBody -->
 						</td>
