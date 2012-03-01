6eb1f8ca11a4c24bf37fddb18c3263fbd235393b
Matthew Flaschen <matthew.flaschen@gatech.edu>
2012-02-29 23:01:19 -0500

    Also activate in userspace until issue 93 is fixed.

--- ProveIt_Wikipedia.js
+++ ProveIt_Wikipedia.js
@@ -353,7 +353,7 @@ window.proveit = jQuery.extend({
 	isSupportedEditPage : function()
 	{
 	        // "Regular" article or Wikipedia:Sandbox (exception for testing).  Also, must be edit or preview mode
-	        return (wgCanonicalNamespace == '' || wgPageName == 'Wikipedia:Sandbox') && (wgAction == 'edit' || wgAction == 'submit');
+	        return (wgCanonicalNamespace == '' || wgCanonicalNamespace == 'User' || wgPageName == 'Wikipedia:Sandbox') && (wgAction == 'edit' || wgAction == 'submit');
 	},
 
 	/**
