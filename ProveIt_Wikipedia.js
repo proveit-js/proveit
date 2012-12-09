7c6a5f247154d836aa19c67362cc11ba2322c6de
Matthew Flaschen <matthew.flaschen@gatech.edu>
2012-12-09 03:35:22 -0500

    Make dependency on jQuery.textSelection explicit.

--- ProveIt_Wikipedia.js
+++ ProveIt_Wikipedia.js
@@ -620,7 +620,7 @@ window.proveit = $.extend({
 	{
 		addOnloadHook(function()
 		{
-			var dependencies = ['jquery.ui.tabs', 'jquery.ui.button', 'jquery.effects.highlight'];
+			var dependencies = ['jquery.ui.tabs', 'jquery.ui.button', 'jquery.effects.highlight', 'jQuery.textSelection'];
 			mw.loader.using(dependencies, function()
 			{
 				try
