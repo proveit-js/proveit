d378abf318a3530433c6fc048dec91586c9e5b8e
Matthew Flaschen <matthew.flaschen@gatech.edu>
2013-03-11 03:17:09 -0400

    Use protocol-relative URLs.

--- ProveIt_userscript_loader.js
+++ ProveIt_userscript_loader.js
@@ -1,5 +1,5 @@
 // Loader to include CSS and JS, so main script does not have to concern itself with loading CSS.
 // In the gadget version, ResourceLoader loads the CSS instead.
 
-mw.loader.load( 'https://en.wikipedia.org/w/index.php?title=User:ProveIt_GT/ProveIt_main.js&action=raw&ctype=text/javascript' );
-mw.loader.load( 'https://en.wikipedia.org/w/index.php?title=User:ProveIt_GT/ProveIt.css&action=raw&ctype=text/css', 'text/css' );
+mw.loader.load( '//en.wikipedia.org/w/index.php?title=User:ProveIt_GT/ProveIt_main.js&action=raw&ctype=text/javascript' );
+mw.loader.load( '//en.wikipedia.org/w/index.php?title=User:ProveIt_GT/ProveIt.css&action=raw&ctype=text/css', 'text/css' );
